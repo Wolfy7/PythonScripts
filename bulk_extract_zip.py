0f60af1f163fc50c1f58bf843a3b77c7ef08a151
André Decher <andre.decher@gmail.com>
2021-08-13 16:28:36 +0200

    Unpacking to folder and some info output

--- bulk_extract_zip.py
+++ bulk_extract_zip.py
@@ -5,11 +5,18 @@ Unpacks all zip archives found in the folder
 from zipfile import ZipFile
 import glob
 import sys
+import os
 
 folder = '.'
 if len(sys.argv) > 1:
     folder = sys.argv[1]
 
+archive_counter = 0
 for zip_file in glob.glob(folder + '\*.zip'):
+    folder_name, extension = os.path.splitext(os.path.basename(zip_file))
+    print(f"Unpacking {folder_name}{extension} ...")
     with ZipFile(zip_file, 'r') as zip:
-        zip.extractall()
+        zip.extractall(folder + "\\" + folder_name)
+    archive_counter += 1
+
+print(f"Finished unpacking {archive_counter} archive(s)")
